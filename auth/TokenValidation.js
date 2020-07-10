04b2cc29db5b5d851f6b35d8e2c2a3a4cbd1a050
Facundo Moreno <facundoivanmoreno1@gmail.com>
2020-07-10 17:26:33 -0300

    access control allow headers methods

---
+++ auth/TokenValidation.js
@@ -0,0 +1,31 @@
+const { verify, decode } = require("jsonwebtoken");
+module.exports = {
+    checkToken: (req, res, next) => {
+        let token = req.get("authorization");
+        if (token) {
+            token = token.slice(7);
+            verify(token, process.env.JSONTOKEN_KEY, (err, decoded) => {
+                if (err) {
+                    res.status(401).json({
+                        success: 0,
+                        message: "Invalid Token",
+                    });
+                } else {
+                    next();
+                }
+            });
+        } else {
+            res.status(403).json({
+                success: 0,
+                message: "Access denied! Unauthorized user",
+            });
+        }
+    },
+
+    decodeToken: (req) => {
+        token = req.headers.authorization.split(" ");
+        token = decode(token[1]);
+        console.log(token);
+        return token;
+    }
+};
